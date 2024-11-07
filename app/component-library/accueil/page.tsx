346d16b51190fe7003306e7a1e4d3f5114952129
Delgorithm <delgorithm@gmail.com>
2024-11-07 12:07:56 +0100

    layout for the page test

--- app/component-library/accueil/page.tsx
+++ app/component-library/accueil/page.tsx
@@ -1,3 +1,14 @@
+import ButtonCTA from "@/components/component-library/ui/button-cta";
+import GridBentoLanding from "@/components/component-library/ui/grid-bento-landing";
+
 export default function LandingPage() {
-	return <p>Landing Page</p>;
+	return (
+		<div>
+			<p>Landing Page</p>
+
+			<ButtonCTA text="Se connecter" />
+			<ButtonCTA text="Essayer gratuitement" />
+			<GridBentoLanding />
+		</div>
+	);
 }
