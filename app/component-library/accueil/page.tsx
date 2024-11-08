49e0dc7e132faf60f0cdf6e035de5e273563133c
Delgorithm <delgorithm@gmail.com>
2024-11-08 15:05:57 +0100

    app/component-library/accueil/page.tsx

--- app/component-library/accueil/page.tsx
+++ app/component-library/accueil/page.tsx
@@ -1,14 +1,42 @@
-import ButtonCTA from "@/components/component-library/ui/button-cta";
+import Functionalities from "@/components/component-library/functionalities";
+import ContactUs from "@/components/component-library/ui/contact-us";
+import Faq from "@/components/component-library/ui/faq";
 import GridBentoLanding from "@/components/component-library/ui/grid-bento-landing";
+import Hero from "@/components/component-library/ui/hero";
+import Navbar from "@/components/component-library/ui/navbar";
+import Pricing from "@/components/component-library/ui/pricing";
+import RoadLightBeam from "@/components/component-library/ui/road-light-beam";
+import SocialProof from "@/components/component-library/ui/social-proof";
+import Image from "next/image";
 
 export default function LandingPage() {
 	return (
-		<div>
-			<p>Landing Page</p>
-
-			<ButtonCTA text="Se connecter" />
-			<ButtonCTA text="Essayer gratuitement" />
-			<GridBentoLanding />
+		<div className="flex flex-col bg-black gap-24 relative">
+			<Image
+				src="/images/component-library/lux-before.svg"
+				height={1000}
+				width={1000}
+				alt="Light"
+				className="absolute pointer-events-none"
+			/>
+			<Image
+				src="/images/component-library/lux-after.svg"
+				height={1000}
+				width={1000}
+				alt="Light"
+				className="absolute pointer-events-none"
+			/>
+			<div className="flex flex-col gap-24 z-10">
+				<Navbar />
+				<Hero />
+				<SocialProof />
+				<Functionalities />
+				<GridBentoLanding />
+				<RoadLightBeam />
+				<Pricing />
+				<Faq />
+				<ContactUs />
+			</div>
 		</div>
 	);
 }
