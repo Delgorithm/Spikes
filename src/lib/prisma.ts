ade20615da0e97d0ef6a2bd5f4f1ef0fc75c2e58
Delgorithm <delgorithm@gmail.com>
2024-11-13 20:01:04 +0100

    fix: new prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,27 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prismaClientSingleton = () => {
-// 	return new PrismaClient({
-// 		datasources: {
-// 			db: {
-// 				url: process.env.DATABASE_URL,
-// 			},
-// 		},
-// 	});
-// };
-
-// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-// const globalForPrisma = globalThis as unknown as {
-// 	prisma: PrismaClientSingleton | undefined;
-// };
-
-// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-// export default prisma;
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 console.log(
@@ -40,7 +16,7 @@ const prismaClientSingleton = () => {
 		});
 	} catch (error) {
 		console.error("Error initializing Prisma Client:", error);
-		throw error; // Rejeter l'erreur pour qu'elle soit détectée dans les logs
+		throw error;
 	}
 };
 
