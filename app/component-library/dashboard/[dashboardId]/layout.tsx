70e7304ab39eaf596e93266971450fc955055511
Delgorithm <delgorithm@gmail.com>
2024-11-14 23:39:23 +0100

    page + layout

---
+++ app/component-library/dashboard/[dashboardId]/layout.tsx
@@ -0,0 +1,22 @@
+import NavbarPasteDashboard from "@/components/component-library/dashboard/ui/navbar-paste-dashboard";
+import SidebarDashboard from "@/components/component-library/dashboard/ui/sidebar-paste-dashboard";
+
+export default function DashboardIdLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className="h-screen grid grid-cols-custom-dashboard-paste-columns grid-rows-custom-dashboard-paste-rows">
+			<div className="col-span-2 row-start-1 bg-[#121212] border-b-[1px] border-[#1D1D1D]">
+				<NavbarPasteDashboard />
+			</div>
+
+			<div className="row-start-2 col-start-1 bg-[#121212] border-r-[1px] border-[#1D1D1D]">
+				<SidebarDashboard />
+			</div>
+
+			<div className="row-start-2 col-start-2 bg-[#1D1D1D]">{children}</div>
+		</section>
+	);
+}
--- app/component-library/dashboard/[dashboardId]/page.tsx
+++ app/component-library/dashboard/[dashboardId]/page.tsx
@@ -4,7 +4,6 @@ import { useSession } from "next-auth/react";
 
 export default function DashboardUserPage() {
 	const { data: session, status } = useSession();
-	console.log(session);
 	return (
 		<section>
 			<p>Salut {session?.user.name}</p>
