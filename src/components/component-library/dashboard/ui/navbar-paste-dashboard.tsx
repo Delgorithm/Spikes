cfa638251306f3324674dbf79ffe6e5bf440e805
Delgorithm <delgorithm@gmail.com>
2024-11-15 15:37:54 +0100

    feat: navbar

--- src/components/component-library/dashboard/ui/navbar-paste-dashboard.tsx
+++ src/components/component-library/dashboard/ui/navbar-paste-dashboard.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Minus, Search, User } from "lucide-react";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NavbarPasteDashboard() {
@@ -10,31 +19,74 @@ export default function NavbarPasteDashboard() {
 
 	const navItems = [
 		{
-			url: "paste-icon.svg",
+			url: "logo.svg",
+			link: `/component-library/dashboard/${userId}/`,
+			alt: "Icon de Paste",
 		},
 		{
 			title: "Library",
-			link: `/dashboard/${userId}/library`,
+			link: `/component-library/dashboard/${userId}/library`,
 		},
 		{
 			title: "Extensions",
-			link: `/dashboard/${userId}/extensions`,
+			link: `/component-library/dashboard/${userId}/extensions`,
 		},
 		{
 			title: "Community",
-			link: `/dashboard/${userId}/community`,
+			link: `/component-library/dashboard/${userId}/community`,
 		},
 		{
 			title: "Membership",
-			link: `/dashboard/${userId}/membership`,
+			link: `/component-library/dashboard/${userId}/membership`,
 		},
 	];
 
+	const iconItem = navItems.slice(0, 1);
+	const navigationItems = navItems.slice(1, 5);
+
 	return (
-		<nav>
+		<nav
+			className={cn(
+				"text-white flex items-center justify-between w-full font-light",
+				"lg:p-8"
+			)}>
 			<ul>
-				<li>Library</li>
+				{iconItem.map((item) => (
+					<Link href={item.link}>
+						<Image
+							src={`/images/${item.url}`}
+							width={100}
+							height={100}
+							alt="Logo"
+						/>
+					</Link>
+				))}
 			</ul>
+			<Minus className="rotate-90 h-[0.5px] bg-[#1D1D1D]" />
+			<ul className="flex items-center gap-8">
+				{navigationItems.map((item) => (
+					<li key={item.title}>{item.title}</li>
+				))}
+			</ul>
+			<Minus className="rotate-90 h-[0.5px] bg-[#1D1D1D]" />
+			<div className="relative">
+				<Search className="stroke-[#7E7F81] stroke-[0.5px] absolute top-2.5 left-4 size-4" />
+				<Input
+					className={cn(
+						"bg-[#1D1D1D] pl-11 border-[#1D1D1D] placeholder:text-[#7E7F81]",
+						"lg:h-[37px] lg:w-[680px]"
+					)}
+					placeholder="Search for specific components, sections, wireframes, and more..."
+				/>
+			</div>
+			<Minus className="rotate-90 h-[0.5px] bg-[#1D1D1D]" />
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<div className="size-9 flex items-center justify-center rounded-[6px] border-[0.5px] border-[#1D1D1D] bg-[#121212]">
+						<User />
+					</div>
+				</DropdownMenuTrigger>
+			</DropdownMenu>
 		</nav>
 	);
 }
