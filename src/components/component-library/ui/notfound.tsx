017f7f248123e7898936aeccc9dacec4896d178d
Delgorithm <delgorithm@gmail.com>
2024-11-21 19:56:21 +0100

    new component

---
+++ src/components/component-library/ui/notfound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<section className="flex flex-col justify-center items-center w-full h-full gap-4  text-white">
+			<h1 className="text-6xl">404</h1>
+			<p>Not Found</p>
+		</section>
+	);
+}
