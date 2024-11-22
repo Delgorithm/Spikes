05ebfb140b08f593293057b8b427bf6608207fdb
Delgorithm <delgorithm@gmail.com>
2024-11-22 16:14:54 +0100

    fix packages

--- src/components/component-library/dashboard/ui/dropdown-profil.tsx
+++ src/components/component-library/dashboard/ui/dropdown-profil.tsx
@@ -17,7 +17,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 type DropdownProfilProps = {
-	handleOpen: () => void;
+	handleOpen?: () => void;
 };
 
 export default function DropdownProfil({ handleOpen }: DropdownProfilProps) {
