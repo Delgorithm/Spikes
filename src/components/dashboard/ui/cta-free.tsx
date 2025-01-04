105888f74e1785bfd519e548d4f2c1bfc1a743a3
Delgorithm <delgorithm@gmail.com>
2025-01-04 23:46:45 +0100

    nvim : Fix : New prop

--- src/components/dashboard/ui/cta-free.tsx
+++ src/components/dashboard/ui/cta-free.tsx
@@ -2,23 +2,29 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CTAFree() {
-	return (
-		<Link
-			href=""
-			className={cn(
-				"flex items-center gap-2 py-1.5 px-4 rounded-lg bg-[#F5F7F9]",
-				"hover:opacity-65 active:translate-y-0.5 transition-all ease-in-out"
-			)}>
-			<Image
-				src="/dashboard/icons/deals_logo.svg"
-				height="0"
-				width="0"
-				sizes="50vw"
-				style={{ width: "auto", height: "auto" }}
-				alt="MyIDeals' logo"
-			/>
-			J'essaye gratuitement
-		</Link>
-	);
+export type CTAFreeProps = {
+  className?: string;
+};
+
+export default function CTAFree({ className }: CTAFreeProps) {
+  return (
+    <Link
+      href=""
+      className={cn(
+        "flex items-center gap-2 py-1.5 px-4 rounded-lg bg-[#F5F7F9]",
+        "hover:opacity-65 active:translate-y-0.5 transition-all ease-in-out",
+        className,
+      )}
+    >
+      <Image
+        src="/dashboard/icons/deals_logo.svg"
+        height="0"
+        width="0"
+        sizes="50vw"
+        style={{ width: "auto", height: "auto" }}
+        alt="MyIDeals' logo"
+      />
+      J'essaye gratuitement
+    </Link>
+  );
 }
