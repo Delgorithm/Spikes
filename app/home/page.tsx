7c988a6c72271ef87d3acd2e43d33f1a87070f2e
Delgorithm <delgorithm@gmail.com>
2025-01-06 18:24:12 +0100

    nvim : new pages

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,18 +2,18 @@ import { Card } from "@/components/ui/card";
 import Link from "next/link";
 
 export default function HomePage() {
-	return (
-		<section className="flex flex-col justify-center items-center gap-4 h-screen ">
-			<Link href="/component-library/accueil" prefetch={true}>
-				<Card className="px-8 py-14 text-center hover:bg-neutral-100 hover:scale-105 transition-all ease-in-out">
-					Librairie de composants
-				</Card>
-			</Link>
-			<Link href="/dashboard/accueil" prefetch={true}>
-				<Card className="px-20 py-14 text-center hover:bg-neutral-100 hover:scale-105 transition-all ease-in-out">
-					Dashboard
-				</Card>
-			</Link>
-		</section>
-	);
+  return (
+    <section className="flex flex-col justify-center items-center gap-4 h-screen ">
+      <Link href="/component-library/accueil" prefetch={true}>
+        <Card className="px-8 py-14 text-center hover:bg-neutral-100 hover:scale-105 transition-all ease-in-out">
+          Librairie de composants
+        </Card>
+      </Link>
+      <Link href="/myideals/accueil" prefetch={true}>
+        <Card className="px-20 py-14 text-center hover:bg-neutral-100 hover:scale-105 transition-all ease-in-out">
+          Dashboard
+        </Card>
+      </Link>
+    </section>
+  );
 }
