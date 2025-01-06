15b4b712fd0fe906739cd89716179f6d0f7fa2fa
Delgorithm <delgorithm@gmail.com>
2025-01-06 18:24:28 +0100

    fix redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/home",
-				permanent: false,
-			},
-			{
-				source: "/component-library",
-				destination: "/component-library/accueil",
-				permanent: false,
-			},
-		];
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "assets.vercel.com",
-				port: "",
-				pathname: "/image/upload/**",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-			},
-		],
-	},
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+      },
+      {
+        source: "/component-library",
+        destination: "/component-library/accueil",
+        permanent: false,
+      },
+      {
+        source: "/myideals",
+        destination: "/myideals/accueil",
+        permanent: false,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
