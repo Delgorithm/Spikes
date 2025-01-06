82f5ac0711e8531015ae85cb6e674b7c6b3b704a
Delgorithm <delgorithm@gmail.com>
2025-01-06 14:58:14 +0100

    nvim : Minor fixes CSS

--- src/components/dashboard/design/functionalities-design.tsx
+++ src/components/dashboard/design/functionalities-design.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function FunctionalitiesDesgin() {
   return (
-    <section className="grid grid-cols-3 grid-rows-1 gap-[15px]">
+    <section className="flex w-full justify-between">
       <div className="bg-[#F2F2F2] h-[400px] w-[346px] border-[#7E7F81] border-[0.5px] rounded-xl">
         <div className="h-[300px] border-b-[0.5px] border-[#7E7F81] w-full flex flex-col justify-center items-center gap-3">
           <div className="flex items-center gap-4 bg-gradient-to-t from-[#0F1420] to-[#161A25] text-white px-10 py-4 rounded-xl">
@@ -22,11 +22,13 @@ export default function FunctionalitiesDesgin() {
           <p className="text-xl">Rapidité et simplicité</p>
         </div>
       </div>
+
       <div className="bg-[#F2F2F2] h-[400px] w-[346px] border-[#7E7F81] border-[0.5px] rounded-xl">
         <div className="h-[300px] border-b-[0.5px] border-[#7E7F81] w-full"></div>
       </div>
+
       <div className="bg-[#F2F2F2] h-[400px] w-[346px] border-[#7E7F81] border-[0.5px] rounded-xl">
-        <div className="h-[300px] border-b-[0.5px] border-[#7E7F81] flex items-center justify-center animate-pingCustom">
+        <div className="h-[300px] border-[#7E7F81] flex items-center justify-center animate-pingCustom">
           <div className="flex justify-center items-center h-5/6 w-[70%] rounded-full border-[0.5px] border-black shadow-xl animate-pingCustom">
             <div className="flex justify-center items-center h-[70%] w-[70%] rounded-full border-[0.5px] border-black shadow-xl animate-pingCustom">
               <div className="flex justify-center items-center h-[50%] w-[50%] rounded-full border-[0.5px] border-black shadow-xl animate-pingCustom">
@@ -43,7 +45,7 @@ export default function FunctionalitiesDesgin() {
             </div>
           </div>
         </div>
-        <div className="h-[100px] w-full flex justify-center items-center">
+        <div className="h-[100px]  border-t-[0.5px] border-[#7E7F81] w-full flex justify-center items-center">
           <p className="text-xl">Au centre de vos suivis</p>
         </div>
       </div>
