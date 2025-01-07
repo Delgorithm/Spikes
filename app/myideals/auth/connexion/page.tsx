5121f8e60ca0049b8544fba5529509dff3ed8ad5
Delgorithm <delgorithm@gmail.com>
2025-01-07 21:55:13 +0100

    nvim : connexion page in progress

--- app/myideals/auth/connexion/page.tsx
+++ app/myideals/auth/connexion/page.tsx
@@ -1,3 +1,16 @@
+import FormConnection from "@/features/auth/form-connection-myideals";
+
 export default function Connexion() {
-  return <section>Connexion</section>;
+  return (
+    <section className="grid grid-cols-2 h-screen">
+      <div className="h-full flex flex-col justify-center items-center">
+        <h1>Connexion</h1>
+        <p>Bon retour !</p>
+        <FormConnection />
+      </div>
+      <div className="h-full flex flex-col justify-center items-center">
+        Image
+      </div>
+    </section>
+  );
 }
