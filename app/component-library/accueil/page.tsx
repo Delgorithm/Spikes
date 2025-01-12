ffb44a4c5405a8ee0f972d293c22233bb9519e94
Delgorithm <delgorithm@gmail.com>
2025-01-12 18:17:24 +0100

    fix : css

--- app/component-library/accueil/page.tsx
+++ app/component-library/accueil/page.tsx
@@ -10,35 +10,36 @@ import SocialProof from "@/components/component-library/ui/social-proof";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-const lux = "/images/component-library/lux.png";
+const lux = "/component-library/lux.png";
 
 export default function LandingPage() {
-	return (
-		<div
-			className={cn(
-				"flex flex-col bg-neutral-950 relative noise",
-				"watch-sm:gap-12 xl:gap-24"
-			)}>
-			<Image
-				src={lux}
-				width={1000}
-				height={1000}
-				priority={false}
-				alt="Light"
-				className={cn("absolute pointer-events-none z-[2]", "block")}
-			/>
+  return (
+    <div
+      className={cn(
+        "flex flex-col bg-neutral-950 relative noise overflow-hidden",
+        "watch-sm:gap-12 xl:gap-24",
+      )}
+    >
+      <Image
+        src={lux}
+        width={1000}
+        height={1000}
+        priority={false}
+        alt="Light"
+        className={cn("absolute pointer-events-none z-[2]", "block")}
+      />
 
-			<div className={cn("flex flex-col z-10", "watch-sm:gap-16 xl:gap-24")}>
-				<Navbar />
-				<Hero />
-				<SocialProof />
-				<Functionalities />
-				<GridBentoLanding />
-				<RoadLightBeam />
-				<Pricing />
-				<Faq />
-				<ContactUs />
-			</div>
-		</div>
-	);
+      <div className={cn("flex flex-col z-10", "watch-sm:gap-16 xl:gap-24")}>
+        <Navbar />
+        <Hero />
+        <SocialProof />
+        <Functionalities />
+        <GridBentoLanding />
+        <RoadLightBeam />
+        <Pricing />
+        <Faq />
+        <ContactUs />
+      </div>
+    </div>
+  );
 }
